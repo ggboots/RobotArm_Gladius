b808d50865af6ad3f51d676a28f1fa02abd47c9f
ggboots <ggboots.99@gmail.com>
2024-06-24 20:14:11 +1000

    initial commit

---
+++ robot_setup.cpp
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+// Parameters
+// -> Default position
+//  -> Min and Max Motion
+// Function for each motion of the robot
+// How those function coincide when using
+
+void gladiusSetup(){
+    
+}
+
+// ShoulderArm
+// elbowArm
